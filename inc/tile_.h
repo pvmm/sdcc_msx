87dfaa411645953be187223247ccb35df8ba9ea9
Pedro de Medeiros <pedro.medeiros@gmail.com>
2021-07-23 10:03:44 -0300

    update tile_.{c|h} files

--- inc/tile_.h
+++ inc/tile_.h
@@ -16,17 +16,17 @@ typedef u8x8_t tileset_t[256];
 
 
 /**
- * Tell library that tile graphics and colors are interlaced.
+ * Init tileset library and define if tile graphics and colors are interlaced.
  * Note: in interlaced mode, half of the 256 tiles are used for every other frame.
  */
-void g2_set_interlaced (bool b);
+void g2_init_tileset (bool interlaced);
 
 
 /**
  * Check if library is in interlaced tile graphics and colors mode.
  * Note: in interlaced mode, half of the 256 tiles are used for every other frame.
  */
-bool g2_get_interlaced (void);
+bool g2_is_interlaced (void);
 
 
 /**
@@ -39,34 +39,45 @@ void g2_wipe_tileset (tileset_t set);
  * Set single tile in tileset.
  */
 void g2_set_tile
-(
-    row_page_flags pages,       // pages where this tileset is active
-    uint8_t index,              // index in ASCII table
-    const u8x8_t shape,         // shape of tile
-    const u8x8_t color          // color of tile
-);
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t shape,         // shape of tile
+        const u8x8_t color          // color of tile
+    );
 
 
 /**
  * Change color of tile in tileset. This affects only the next printed tiles.
  */
 void g2_set_tile_color
-(
-    row_page_flags pages,       // pages where this tileset is active
-    uint8_t index,              // index in ASCII table
-    const u8x8_t color          // color of tile
-);
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t color          // color of tile
+    );
 
 
 /**
  * Change shape of tile in tileset. This affects only the next printed tiles.
  */
 void g2_set_tile_shape
-(
-    row_page_flags pages,       // pages where this tileset is active
-    uint8_t index,              // index in ASCII table
-    const u8x8_t shape          // shape of tile
-);
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t shape          // shape of tile
+    );
+
+
+/**
+ * Set a tile pair to be used in interlaced mode.
+ */
+void g2_set_tile_pairing
+    (
+        uint8_t assoc_code,
+        uint8_t ascii_code1,
+        uint8_t ascii_code2
+    );
 
 
 /**
@@ -75,11 +86,11 @@ void g2_set_tile_shape
  * Note: this is most useful when using tileset as font.
  */
 INLINE void g2_set_font
-(
-	row_page_flags pages,       // pages where this tileset is active
-    const tileset_t set,              // tileset to setup
-	const u8x8_t color          // colors to use for the entire tileset
-)
+    (
+    	row_page_flags pages,       // pages where this tileset is active
+        const tileset_t set,              // tileset to setup
+    	const u8x8_t color          // colors to use for the entire tileset
+    )
 {
 	for (uint16_t index = 0; index < 256; index++)
         g2_set_tile (pages, index, set[index], color);
@@ -90,11 +101,11 @@ INLINE void g2_set_font
  * Set all tiles at once.
  */
 INLINE void g2_set_tileset
-(
-	row_page_flags pages,          // pages where this font is active
-	const tileset_t shapes,   // tiles shapes
-	const tileset_t colors    // tiles colors
-)
+    (
+    	row_page_flags pages,          // pages where this font is active
+    	const tileset_t shapes,   // tiles shapes
+    	const tileset_t colors    // tiles colors
+    )
 {
 	for (uint16_t index = 0; index < 256; index++)
         g2_set_tile (pages, index, shapes[index], colors[index]);
@@ -108,6 +119,9 @@ void g2_get_pos (uint8_t *x, uint8_t *y);
 
 
 void g2_put_tile (uint8_t index);
+
+
+void g2_put_interlaced (uint8_t index0, uint8_t index1);
 
 
 void g2_put_buffer (const char *buf, uint16_t len);
--- src/tile_.c
+++ src/tile_.c
@@ -1,23 +1,30 @@
 #include <tms99X8.h>
 #include <tile_.h>
+#include <stdlib.h>
 //#include <assert.h>
 
 
+typedef uint8_t tile_pair_t[256][2];
+
 static uint8_t pos_x = 0, pos_y = 0;
 
+static bool _interlaced = false;
 
-static bool interlaced = false;
+/**
+ * Composite tilemap from different frames.
+ */
+static tile_pair_t tile_pair;
 
 
-void g2_set_interlaced (bool b)
+void g2_init_tileset (bool interlaced)
 {
-    interlaced = b;
+    _interlaced = interlaced;
 }
 
 
-bool g2_get_interlaced (void)
+bool g2_is_interlaced (void)
 {
-    return interlaced;
+    return _interlaced;
 }
 
 
@@ -29,32 +36,33 @@ void g2_wipe_tileset (tileset_t set)
 
 
 void g2_set_tile
-(
-    row_page_flags pages,       // pages where this tileset is active
-    uint8_t index,              // index in ASCII table
-    const u8x8_t shape,         // shape of tile
-    const u8x8_t color          // color of tile
-)
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t shape,         // shape of tile
+        const u8x8_t color          // color of tile
+    )
 {
-	uint16_t tile_address;
+    uint16_t tile_address;
 
-	for (uint8_t page = 0; page < 3; page++)
+    for (uint8_t page = 0; page < 3; page++)
     {
         if ((pages & (1 << page)) == 0) continue;
 
-		tile_address = (( (uint16_t) page) << 11) + (( (uint16_t) index) << 3);
-		TMS99X8_memcpy (MODE2_ADDRESS_PG + tile_address, shape, 8);
-		TMS99X8_memcpy (MODE2_ADDRESS_CT + tile_address, color, 8);
-	}
+        tile_address = (( (uint16_t) page) << 11) + (( (uint16_t) index) << 3);
+        printf("tile address = %x (%i)\n", 0xffff & tile_address, index);
+        TMS99X8_memcpy (MODE2_ADDRESS_PG + tile_address, shape, 8);
+        TMS99X8_memcpy (MODE2_ADDRESS_CT + tile_address, color, 8);
+    }
 }
 
 
 void g2_set_tile_color
-(
-    row_page_flags pages,       // pages where this tileset is active
-    uint8_t index,              // index in ASCII table
-    const u8x8_t color          // color of tile
-)
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t color          // color of tile
+    )
 {
 	uint16_t tile_address;
 
@@ -69,11 +77,11 @@ void g2_set_tile_color
 
 
 void g2_set_tile_shape
-(
-    row_page_flags pages,       // pages where this tileset is active
-    uint8_t index,              // index in ASCII table
-    const u8x8_t shape          // shape of tile
-)
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t shape          // shape of tile
+    )
 {
 	uint16_t tile_address;
 
@@ -87,6 +95,18 @@ void g2_set_tile_shape
 }
 
 
+void g2_set_tile_pairing
+    (
+        uint8_t assoc_code,
+        uint8_t ascii_code1,
+        uint8_t ascii_code2
+    )
+{
+    tile_pair[assoc_code][0] = ascii_code1;
+    tile_pair[assoc_code][1] = ascii_code2;
+}
+
+
 void g2_set_pos (const uint8_t x, const uint8_t y) { pos_x = x; pos_y = y; }
 
 
@@ -97,12 +117,28 @@ void g2_put_tile (uint8_t index)
 {
 	uint16_t address = (( (uint16_t) pos_y) << 5) + pos_x;
 
-	TMS99X8_memset (MODE2_ADDRESS_PN0 + address, index, 1);
-
-    if (interlaced)
+    if (_interlaced)
     {
-        TMS99X8_memset (MODE2_ADDRESS_PN1 + address, index + 128, 1);
+        TMS99X8_memset (MODE2_ADDRESS_PN0 + address, tile_pair[index][0], 1);
+        TMS99X8_memset (MODE2_ADDRESS_PN1 + address, tile_pair[index][1], 1);
     }
+    else
+    {
+        TMS99X8_memset (MODE2_ADDRESS_PN0 + address, index, 1);
+    }
+
+	pos_x++;
+	if (pos_x == 32) { pos_x = 0; pos_y++; };
+	if (pos_y == 24) { pos_y = 0; };
+}
+
+
+void g2_put_interlaced (uint8_t ascii_code1, uint8_t ascii_code2)
+{
+	uint16_t address = (( (uint16_t) pos_y) << 5) + pos_x;
+
+	TMS99X8_memset (MODE2_ADDRESS_PN0 + address, ascii_code1, 1);
+    TMS99X8_memset (MODE2_ADDRESS_PN1 + address, ascii_code2, 1);
 
 	pos_x++;
 	if (pos_x == 32) { pos_x = 0; pos_y++; };
