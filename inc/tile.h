bf6f900cacefa2fee9261bb2950b1329a0fccd44
Pedro de Medeiros <pedro.medeiros@gmail.com>
2021-07-25 16:22:48 -0300

    replacing old tile.{c|h} files with tile_.{c|h}

--- inc/tile.h
+++ inc/tile.h
@@ -1,37 +1,137 @@
 #pragma once
-#include <msxhal.h>
 #include <tms99X8.h>
 
-////////////////////////////////////////////////////////////////////////
-// Tile Manager API
-//
-// Used to reserve and free tiles.
-//
-typedef uint8_t TileIdx;
-
-void    Tiles_init();
-TileIdx Tiles_findAndAllocate(RowPageIdx rowPageIdx);
-void    Tiles_alloc   (RowPageIdx rowPageIdx, TileIdx tileIdx);
-void    Tiles_free    (RowPageIdx rowPageIdx, TileIdx tileIdx);
-INLINE void  Tiles_freeAll() { Tiles_init(); }
 
 /*
-////////////////////////////////////////////////////////////////////////
-// Proportional Font API
-//
-// Text API where Tiles are generated for each message.
-//
+ * Graphics 2 tileset
+ */
 
-bool MODE2_PS_renderMsg(
-	EM2_PGpage m2pgpage,
-	TileIdx *slim, 
-	TileIdx *bold, 
-	const char *msg, 
-	const TFont font, 
-	const U8x8 colorSlim,
-	const U8x8 colorBold,
-	uint8_t padding);
 
-void M2_PS_freeMsg(TileIdx *msg);
-void M2_PS_print(EM2_Buffer buffer, uint8_t x, uint8_t y, TileIdx *msg);
-*/
+/* Standard POSIX-style types */
+typedef EM2_RowPageFlags row_page_flags;
+typedef U8x8 u8x8_t;
+
+
+/* A 256 chars tileset represents a third of the screen in GRAPHICS 2 */
+typedef u8x8_t tileset_t[256];
+
+
+/**
+ * Init tileset library and define if tile graphics and colors are interlaced.
+ * Note: in interlaced mode, half of the 256 tiles are used for every other frame.
+ */
+void g2_init_tileset (bool interlaced);
+
+
+/**
+ * Check if library is in interlaced tile graphics and colors mode.
+ * Note: in interlaced mode, half of the 256 tiles are used for every other frame.
+ */
+bool g2_is_interlaced (void);
+
+
+/**
+ * Wipe tileset memory
+ */
+void g2_wipe_tileset (tileset_t set);
+
+
+/**
+ * Set single tile in tileset.
+ */
+void g2_set_tile
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t shape,         // shape of tile
+        const u8x8_t color          // color of tile
+    );
+
+
+/**
+ * Change color of tile in tileset. This affects only the next printed tiles.
+ */
+void g2_set_tile_color
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t color          // color of tile
+    );
+
+
+/**
+ * Change shape of tile in tileset. This affects only the next printed tiles.
+ */
+void g2_set_tile_shape
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t shape          // shape of tile
+    );
+
+
+/**
+ * Set a tile pair to be used in interlaced mode.
+ */
+void g2_set_tile_pairing
+    (
+        uint8_t assoc_code,
+        uint8_t ascii_code1,
+        uint8_t ascii_code2
+    );
+
+
+/**
+ * Set all tiles at once with the sames colors (2 colors per line).
+ *
+ * Note: this is most useful when using tileset as font.
+ */
+INLINE void g2_set_font
+    (
+    	row_page_flags pages,       // pages where this tileset is active
+        const tileset_t set,              // tileset to setup
+    	const u8x8_t color          // colors to use for the entire tileset
+    )
+{
+	for (uint16_t index = 0; index < 256; index++)
+        g2_set_tile (pages, index, set[index], color);
+}
+
+
+/**
+ * Set all tiles at once.
+ */
+INLINE void g2_set_tileset
+    (
+    	row_page_flags pages,          // pages where this font is active
+    	const tileset_t shapes,   // tiles shapes
+    	const tileset_t colors    // tiles colors
+    )
+{
+	for (uint16_t index = 0; index < 256; index++)
+        g2_set_tile (pages, index, shapes[index], colors[index]);
+}
+
+
+void g2_set_pos (const uint8_t x, const uint8_t y);
+
+
+void g2_get_pos (uint8_t *x, uint8_t *y);
+
+
+void g2_put_tile (uint8_t index);
+
+
+void g2_put_interlaced (uint8_t index0, uint8_t index1);
+
+
+void g2_put_buffer (const char *buf, uint16_t len);
+
+
+void g2_put_at (uint8_t x, uint8_t y, const char *buf, uint16_t len);
+
+
+void transform_null(const u8x8_t source, u8x8_t target);
+
+
+void transform_flip_x(const u8x8_t source, u8x8_t target);
--- src/tile.c
+++ src/tile.c
@@ -1,211 +1,183 @@
+#include <tms99X8.h>
 #include <tile.h>
-
-static uint8_t tileOccupancyMap[3][32];
-
-////////////////////////////////////////////////////////////////////////
-// Tile Manager API
-//
-// Used to reserve and free tiles.
-//
-
-TileIdx Tiles_findAndAllocate(RowPageIdx rowPageIdx) {
-	
-	uint8_t i, val;
-	uint8_t *p = &tileOccupancyMap[rowPageIdx][0];
-	
-	for (i=0; (i<32) && (*p==0xFF); i++, p++);
-	
-	if (i==32) return 0;
-
-	val = *p;
-	i = i << 3;
-	while (val & 0x01) {
-		i++;
-		val = val >> 1;
-	}
-	
-	Tiles_alloc(rowPageIdx, i);
-	return i;
-}
-
-void    Tiles_alloc   (RowPageIdx rowPageIdx, TileIdx tileIdx) {
-	
-	uint8_t *val = &tileOccupancyMap[rowPageIdx][tileIdx>>3];
-	*val = *val | (1<<(tileIdx&0x07));	
-}
-
-void    Tiles_free    (RowPageIdx rowPageIdx, TileIdx tileIdx) {
-
-	uint8_t *val = &tileOccupancyMap[rowPageIdx][tileIdx>>3];
-	*val = *val & ~(1<<(tileIdx&0x07));	
-}
-
-void    Tiles_init() {
-	
-	uint8_t j=0;
-	for (j=0; j<3; j++) {
-		uint8_t i=32;
-		uint8_t *p = &tileOccupancyMap[j][0];
-		while (i--)
-			*p++ = 0;
-		Tiles_alloc(j,0); // Tile 0 is reserved for signaling the end of a message
-	}
-}
-/*
+#include <stdlib.h>
+//#include <assert.h>
 
 
-// Text rutines
-void uint16ToString(char *msg, uint16_t v, uint8_t enableZeroPad) {
-	
-	register char *p = msg;
-	char c;
-	c = 0; while (v>10000) { c++; v-=10000; } if (c || enableZeroPad) *p++ = '0'+c;
-	c = 0; while (v>1000 ) { c++; v-=1000;  } if (c || enableZeroPad) *p++ = '0'+c;
-	c = 0; while (v>100  ) { c++; v-=100;   } if (c || enableZeroPad) *p++ = '0'+c;
-	c = 0; while (v>10   ) { c++; v-=10;    } if (c || enableZeroPad) *p++ = '0'+c;
-	c = 0; while (v>1    ) { c++; v-=1;     } if (c) *p++ = '0'+c;
-	*p++=0;
+typedef uint8_t tile_pair_t[256][2];
+
+static uint8_t pos_x = 0, pos_y = 0;
+
+static bool _interlaced = false;
+
+/**
+ * Composite tilemap from different frames.
+ */
+static tile_pair_t tile_pair;
+
+
+void g2_init_tileset (bool interlaced)
+{
+    _interlaced = interlaced;
 }
 
 
-
-
-
-
-
-
-
-
-#define INIT(a,v) { register uint8_t value = v; register uint8_t sz = sizeof(a); register uint8_t *p = (uint8_t *)a; while (sz--) *p++=value; }
-
-#define uintXX_t uint16_t
-#define XX (8*sizeof(uintXX_t))
-
-static void pasteCharacter(uintXX_t workAreaSlim[8], uintXX_t workAreaBold[8], const TFont font, uint8_t c, uint8_t maxDisplacement_) {
-
-	uint8_t i;
-	register uint8_t maxDisplacement = maxDisplacement_;
-	register const uint8_t *f = font[c];
-	for (i=0; i<8; i++) {
-		
-		workAreaSlim[i] |= ((uintXX_t)f[i]) << maxDisplacement;
-		
-		workAreaBold[i] |= 
-			(((uintXX_t)f[i]) << (maxDisplacement+1)) | 
-			(((uintXX_t)f[i]) << (maxDisplacement+0)) | 
-			(((uintXX_t)f[i]) << (maxDisplacement-1));
-	}
-
-	for (i=0; i<7; i++) {
-		
-		workAreaBold[i+1] |= ((uintXX_t)f[i]) << (maxDisplacement+1);
-		workAreaBold[i+1] |= ((uintXX_t)f[i]) << (maxDisplacement+0);
-		workAreaBold[i+1] |= ((uintXX_t)f[i]) << (maxDisplacement-1);
-
-		workAreaBold[i] |= ((uintXX_t)f[i+1]) << (maxDisplacement+1);
-		workAreaBold[i] |= ((uintXX_t)f[i+1]) << (maxDisplacement+0);
-		workAreaBold[i] |= ((uintXX_t)f[i+1]) << (maxDisplacement-1);
-	}
+bool g2_is_interlaced (void)
+{
+    return _interlaced;
 }
 
-static uint8_t writeShape(uintXX_t workArea[8], uint8_t freeTiles[256]) {
-	
-	
-	U8x8 shape;
-	uint8_t freeTile = 1;
-	{
-		uint8_t i,j;
-		j = 0;
-		for (i=0; i<8; i++) {
-			shape[i] = workArea[i]>>(XX-8);
-			j = j || shape[i];
-			workArea[i] <<= 8;
-		}
-		if (!j) return 0;
-	}
-	while (freeTile && freeTiles[freeTile]==0) freeTile++;
 
-	TMS99X8_memcpy(ADDRESS_PG + (((uint16_t)freeTile)<<3), shape, 8);
-
-	freeTiles[freeTile] = 0;
-	return freeTile;
+/* interlaced tileset */
+void g2_wipe_tileset (tileset_t set)
+{
+	memset (set, 0, sizeof (tileset_t));
 }
 
-void initRenderedText(uint8_t *slim, uint8_t *bold, uint8_t freeTiles[256], const uint8_t *str, const TFont font, uint8_t padding) {
-	
-	int8_t maxDisplacement = XX-8-padding;
-	uintXX_t workAreaSlim[8];
-	uintXX_t workAreaBold[8];
-	INIT(workAreaSlim,0);
-	INIT(workAreaBold,0);
 
-	if (*str==0) return;
-	{
-		uint8_t c = *str++;
-		pasteCharacter(workAreaSlim,workAreaBold,font,c,maxDisplacement);
-		maxDisplacement -= 8;
-//		printf("A: %d\n",maxDisplacement);
-	}
-	while (*str) {
-		uint8_t c = *str;
-		int8_t mx = min((int8_t)(XX-8), maxDisplacement+6);
-//		printf("MX A: %d %d\n",maxDisplacement, mx);
-		
-		while (maxDisplacement < mx) {
-			uint8_t i;
-			if (maxDisplacement>=0)
-				for (i=0; i<8; i++) 
-					if ((workAreaSlim[i] & (((uintXX_t)font[c][i]) << maxDisplacement)) != 0) 
-						goto out;
-			if (maxDisplacement< 0)
-				for (i=0; i<8; i++) 
-					if ((workAreaSlim[i] & (((uintXX_t)font[c][i]) >> -maxDisplacement)) != 0) 
-						goto out;
-			maxDisplacement++;
-		}
-		out:
+void g2_set_tile
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t shape,         // shape of tile
+        const u8x8_t color          // color of tile
+    )
+{
+    uint16_t tile_address;
 
-//		printf("MX B: %d %d\n\n",maxDisplacement, mx);
-		
-		if (maxDisplacement<=3) {
-			
-			*slim++ = writeShape(workAreaSlim, freeTiles);
-			*bold++ = writeShape(workAreaBold, freeTiles);
-			maxDisplacement += 8;
-			continue;
-		}
-		
-		maxDisplacement -= 3;
-		pasteCharacter(workAreaSlim,workAreaBold,font,c,maxDisplacement);
-		maxDisplacement -= 8;
-		str++;
-	}
-	while (TRUE) {
-		
-		uint8_t vs, vb;
+    for (uint8_t page = 0; page < 3; page++)
+    {
+        if ((pages & (1 << page)) == 0) continue;
 
-		*slim++ = vs = writeShape(workAreaSlim, freeTiles);
-		*bold++ = vb = writeShape(workAreaBold, freeTiles);
-		if (!(vs || vb)) break;
-		
+        tile_address = (( (uint16_t) page) << 11) + (( (uint16_t) index) << 3);
+        printf("tile address = %x (%i)\n", 0xffff & tile_address, index);
+        TMS99X8_memcpy (MODE2_ADDRESS_PG + tile_address, shape, 8);
+        TMS99X8_memcpy (MODE2_ADDRESS_CT + tile_address, color, 8);
+    }
+}
+
+
+void g2_set_tile_color
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t color          // color of tile
+    )
+{
+	uint16_t tile_address;
+
+	for (uint8_t page = 0; page < 3; page++)
+    {
+        if ((pages & (1 << page)) == 0) continue;
+
+		tile_address = (( (uint16_t) page) << 11) + (( (uint16_t) index) << 3);
+		TMS99X8_memcpy (MODE2_ADDRESS_CT + tile_address, color, 8);
 	}
 }
 
 
-void TMS99X8_printStr(uint16_t baseAddress, uint8_t x, uint8_t y, const char *msg) {
+void g2_set_tile_shape
+    (
+        row_page_flags pages,       // pages where this tileset is active
+        uint8_t index,              // index in ASCII table
+        const u8x8_t shape          // shape of tile
+    )
+{
+	uint16_t tile_address;
 
-	uint8_t tiles[32];
-	register uint8_t l = 0;
-	{
-		register const char *src = msg;
-		register uint8_t *dst = &tiles[0];
-		while (TRUE) {
-			register uint8_t v = *src++;
-			if (v==0) break;
-			l++;
-			*dst++ = ascii2tiles[v];
-		}
+	for (uint8_t page = 0; page < 3; page++)
+    {
+        if ((pages & (1 << page)) == 0) continue;
+
+		tile_address = (( (uint16_t) page) << 11) + (( (uint16_t) index) << 3);
+		TMS99X8_memcpy (MODE2_ADDRESS_CT + tile_address, shape, 8);
 	}
-	TMS99X8_memcpy(baseAddress + x+(y<<5),tiles,l);
 }
-*/
+
+
+void g2_set_tile_pairing
+    (
+        uint8_t assoc_code,
+        uint8_t ascii_code1,
+        uint8_t ascii_code2
+    )
+{
+    tile_pair[assoc_code][0] = ascii_code1;
+    tile_pair[assoc_code][1] = ascii_code2;
+}
+
+
+void g2_set_pos (const uint8_t x, const uint8_t y) { pos_x = x; pos_y = y; }
+
+
+void g2_get_pos (uint8_t *x, uint8_t *y) { *x = pos_x; *y = pos_y; }
+
+
+void g2_put_tile (uint8_t index)
+{
+	uint16_t address = (( (uint16_t) pos_y) << 5) + pos_x;
+
+    if (_interlaced)
+    {
+        TMS99X8_memset (MODE2_ADDRESS_PN0 + address, tile_pair[index][0], 1);
+        TMS99X8_memset (MODE2_ADDRESS_PN1 + address, tile_pair[index][1], 1);
+    }
+    else
+    {
+        TMS99X8_memset (MODE2_ADDRESS_PN0 + address, index, 1);
+    }
+
+	pos_x++;
+	if (pos_x == 32) { pos_x = 0; pos_y++; };
+	if (pos_y == 24) { pos_y = 0; };
+}
+
+
+void g2_put_interlaced (uint8_t ascii_code1, uint8_t ascii_code2)
+{
+	uint16_t address = (( (uint16_t) pos_y) << 5) + pos_x;
+
+	TMS99X8_memset (MODE2_ADDRESS_PN0 + address, ascii_code1, 1);
+    TMS99X8_memset (MODE2_ADDRESS_PN1 + address, ascii_code2, 1);
+
+	pos_x++;
+	if (pos_x == 32) { pos_x = 0; pos_y++; };
+	if (pos_y == 24) { pos_y = 0; };
+}
+
+
+void g2_put_buffer (const char *buf, uint16_t size)
+{
+	while (size--> 0)
+		g2_put_tile (*buf++);
+}
+
+
+void g2_put_at (uint8_t x, uint8_t y, const char *buf, uint16_t size)
+{
+	g2_set_pos (x, y);
+	g2_put_buffer (buf, size);
+}
+
+
+void transform_null(const u8x8_t source, u8x8_t target)
+{
+	for (uint8_t i = 0; i < 8; i++)
+		target[i] = source[i];
+}
+
+
+void transform_flip_x(const u8x8_t source, u8x8_t target)
+{
+	for (int i = 0; i < 8; ++i)
+    {
+		target[i]  = (source[i] & 1)  << 7;   // 00000001 => 10000000
+		target[i] |= (source[i] & 2)  << 5;   // 00000010 => 01000000
+		target[i] |= (source[i] & 4)  << 3;   // 00000100 => 00100000
+		target[i] |= (source[i] & 8)  << 1;   // 00001000 => 00010000
+		target[i] |= (source[i] & 16) >> 1;   // 00010000 => 00001000
+		target[i] |= (source[i] & 32) >> 3;   // 00100000 => 00000100
+		target[i] |= (source[i] & 64) >> 5;   // 01000000 => 00000010
+		target[i] |= (source[i] & 128) >> 7;  // 10000000 => 00000001
+	}
+}
